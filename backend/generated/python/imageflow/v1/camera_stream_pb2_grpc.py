7a850d1c75e0fbaf51e97c773e0aa611fda8e67b
Daiichiro Noda <noda724@gmail.com>
2025-07-27 17:09:29 +0900

    ビルドに成功した

--- backend/generated/python/imageflow/v1/camera_stream_pb2_grpc.py
+++ backend/generated/python/imageflow/v1/camera_stream_pb2_grpc.py
@@ -5,7 +5,7 @@ import warnings
 
 from imageflow.v1 import camera_stream_pb2 as imageflow_dot_v1_dot_camera__stream__pb2
 
-GRPC_GENERATED_VERSION = '1.74.0'
+GRPC_GENERATED_VERSION = '1.73.1'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
--- generated/python/imageflow/v1/resize_pb2_grpc.py
+++ generated/python/imageflow/v1/resize_pb2_grpc.py
@@ -6,7 +6,7 @@ import warnings
 from imageflow.v1 import common_pb2 as imageflow_dot_v1_dot_common__pb2
 from imageflow.v1 import resize_pb2 as imageflow_dot_v1_dot_resize__pb2
 
-GRPC_GENERATED_VERSION = '1.74.0'
+GRPC_GENERATED_VERSION = '1.73.1'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
